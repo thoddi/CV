565c32037c2333e6de8f4067bf3d32c7b2d89169
thoddi <thorvardur13@ru.is>
2018-01-15 00:57:15 +0000

    expand ready. Testing click

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -7,27 +7,48 @@ import { Component, OnInit } from '@angular/core';
 export class ExperienceComponent implements OnInit {
 
   wrapper: HTMLElement;
-  textBox: HTMLElement;
+  //buttonArrow: HTMLElement;
+  Element: HTMLElement;
   constructor() {
       this.wrapper = undefined;
-      this.textBox = undefined;
+      //this.buttonArrow = undefined;
+      this.Element = undefined;
   }
 
   ngOnInit() {
   }
 
   expandDescription(e: HTMLElement): void {
+    this.Element = e;
     this.wrapper = <HTMLElement>e.children[2];
-    this.textBox = <HTMLElement>this.wrapper.children[0];
+    //this.buttonArrow = <HTMLElement>e.children[3];
+    let textBox = <HTMLElement>this.wrapper.children[0];
 
-    this.wrapper.style.height = this.textBox.clientHeight + "px";
+    this.wrapper.style.height = textBox.clientHeight + "px";
   }
 
   collapseDescription(e: HTMLElement): void {
     this.wrapper.style.height = "0px";
-
-    this.wrapper = undefined;
-    this.textBox = undefined;
+    this.wrapper = this.Element = undefined;
   }
 
+  onBlockClick(e: HTMLElement): void {
+    if(this.Element != e) {
+      console.log("What's going on here?");
+      this.colOrExp(e);
+    }
+  }
+
+  colOrExp(e: HTMLElement): void {
+    let arrowBtn = <HTMLElement>e.children[3];
+    let classIndex: number = arrowBtn.classList.length;
+    if(arrowBtn.classList[classIndex - 1] == "up") {
+      arrowBtn.classList.remove("up");
+      this.collapseDescription(e);
+    }
+    else {
+      arrowBtn.classList.add("up");
+      this.expandDescription(e);
+    }
+  }
 }
